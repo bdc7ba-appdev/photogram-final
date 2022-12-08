1c2f9866d5f50d48dde6eab5096dddb88754992c
bdc7ba <bcrozier@chicagobooth.edu>
2022-12-08 19:56:03 +0000

    working on photo detail pages, specifically fan stuff

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,4 +12,8 @@
 class Comment < ApplicationRecord
   belongs_to(:photo, { :required => true, :class_name => "Photo", :foreign_key => "photo_id" })
   belongs_to :user, :required => true
+
+  def commenter
+    return User.where({ :id => self.author_id }).at(0)
+  end
 end
