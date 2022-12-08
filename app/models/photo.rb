50776b414b14f3e1a8c7ff21e48cf80227ddd9ab
bdc7ba <bcrozier@chicagobooth.edu>
2022-12-08 18:38:15 +0000

    still formatting

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,6 +12,10 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
+  def poster
+    return User.where({ :id => self.owner_id }).at(0)
+  end
+
   has_many(:comments, { :class_name => "Comment", :foreign_key => "photo_id", :dependent => :destroy })
   has_many(:likes, { :class_name => "Like", :foreign_key => "photo_id", :dependent => :destroy })
   belongs_to(:owner, { :required => true, :class_name => "User", :foreign_key => "owner_id" })
